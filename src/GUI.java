1101e669363d3e565c6f9f350d43ef435715c189
dwfeng2 <dwfeng2@illinois.edu>
2022-02-22 15:40:24 -0600

    Clearning bin and graphics test

--- src/GUI.java
+++ src/GUI.java
@@ -3,10 +3,10 @@ import javax.swing.JFrame;
 
 public class GUI {
     public static void main (String[] args) {
-        int width = 500;
+        int width = 800;
         int height = 500;
         JFrame frame = new JFrame("Menu Screen");
-        frame.setSize(1000, 1000);
+        frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,12 @@
+package src;
 import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("What is your name");
-
 		Scanner input = new Scanner(System.in);
-		
 		String name = input.nextLine();
-		
-		System.out.println("Welcome " + name);		
+		System.out.println("Welcome " + name);	
+		input.close();	
 	}
-
 }
