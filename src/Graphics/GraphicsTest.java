1a8e5fb65cdae6a7f713d3ab3ead787954c67176
Akash Sannasi <sannasi2@illinois.edu>
2022-02-23 14:12:47 -0600

    Integrated JUnit testing into our Game Logic and Graphics components and downloaded JUnit Library

---
+++ src/Graphics/GraphicsTest.java
@@ -0,0 +1,12 @@
+package src.Graphics;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GraphicsTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
