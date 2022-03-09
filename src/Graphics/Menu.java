48d5e4f3f26df2c71856871842cf28a465b7fe49
Akash Sannasi <54860072+3sannasia@users.noreply.github.com>
2022-03-09 11:02:38 -0600

    Add files via upload
    Implemented actions for the Menu class, created functions for the ScreenState to reset JFrame for next level/game

--- src/Graphics/Menu.java
+++ src/Graphics/Menu.java
@@ -1,9 +1,11 @@
 package src.Graphics;
+import src.GameLogic.*;
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.AbstractAction;
+
 /**
  * Starting screen
  * 
@@ -20,23 +22,26 @@ public class Menu extends JFrame implements ActionListener {
     static JMenuItem instructions;
     static JMenuItem previous_level;
     static JMenuBar menu_bar;
+    static JFrame frame;
     public Menu() {
-        JFrame frame = new JFrame("Menu");
+        // Setting up the window and the menu drop down
+        frame = new JFrame("Menu");
         menu_bar = new JMenuBar();
         menu = new JMenu("Play");
-        play = new JMenuItem(new AbstractAction("Start")) {
-            actionPerformed();
-        }
+        frame.getContentPane();
+        play = new JMenuItem("Start");
         instructions = new JMenuItem("Instructions");
         previous_level = new JMenuItem("Previous Level");
-    
+
+        // Adding components to the menu bar
+        menu_bar.add(menu);
         menu.add(play);
         menu.add(instructions);
         menu.add(previous_level);
-        menu_bar.add(menu);
-        JMenuItem.addActionListener(this);
-        JMenuItem.addActionListener(this);
-        JMenuItem.addActionListener(this);
+   
+        play.addActionListener(this);
+        instructions.addActionListener(this);
+        previous_level.addActionListener(this);
         frame.setJMenuBar(menu_bar);
         frame.setSize(500, 500);
         frame.setVisible(true);
@@ -44,31 +49,33 @@ public class Menu extends JFrame implements ActionListener {
 
     //-------- Loads board when start pressed --------//
     public void StartGame() {
-
+        ScreenState start = new ScreenState(frame);
+        start.startGame(new JFrame("Level " + start.getCurrentLevel()));
+        
     }
 
     //-------- Load Previous Level when previous level pressed --------//
     
     public void PreviousLevel() {
-
+        ScreenState previousLevel = new ScreenState(frame);
+        previousLevel.previousLevel(new JFrame("Previous Level"));
     }
 
     //-------- Loads instructions when button pressed --------//
     public void Instructions() {
-
+        //Creates instruction Window
+        // Figure out how to wipe and replace the same JFrame
+        ScreenState instructions = new ScreenState(frame);
+        instructions.displayInstructions(new JFrame("Instructions"));
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("Start")){
+        if(e.getSource() == play){
             StartGame();
-        }else if(e.getActionCommand().equals("Previous Level")){
+        }else if(e.getSource() == instructions){
             Instructions();
-        }else if (e.getActionCommand().equals("Previous Level")){
+        }else if (e.getSource() == previous_level){
             PreviousLevel();
          }
     }
-    public static void main(String[] args){
-        Menu menu = new Menu();
-    }
-    
 }
