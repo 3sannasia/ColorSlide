7b913586d0f0f74748c0ba62641772b476d21f3b
dwfeng2 <dwfeng2@illinois.edu>
2022-04-20 11:04:54 -0500

    Level example in main

--- src/Main.java
+++ src/Main.java
@@ -20,34 +20,33 @@ public class Main {
                 final int WIDTH = 800;
                 final int HEIGHT = 800;
 
-                LevelBoard level;
                 GameBoard frame;
                 Menu menu;
-                try {
-                    menu = new Menu(WIDTH, HEIGHT);
-                    // if (menu.getGameStarted()){
-                    //     level = new LevelBoard("src/Levels/LevelTest.txt", 50, 50);
-                    //     frame = new GameBoard(WIDTH, HEIGHT, level);
-                    //     frame.setSize(WIDTH, HEIGHT);
-                    //     frame.setResizable(false);
-                    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    //     frame.setVisible(true);
+                // try {
+                //     menu = new Menu(WIDTH, HEIGHT);
+                //     // if (menu.getGameStarted()){
+                //     //     level = new LevelBoard("src/Levels/LevelTest.txt", 50, 50);
+                //     //     frame = new GameBoard(WIDTH, HEIGHT, level);
+                //     //     frame.setSize(WIDTH, HEIGHT);
+                //     //     frame.setResizable(false);
+                //     //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //     //     frame.setVisible(true);
     
-                    // } else if (menu.getPreviousLevelPressed()) {
-                    //     level = new LevelBoard("src/Levels/LevelTest.txt", 50,50); // Replace with previous level .txt
-                    //     frame = new GameBoard(WIDTH, HEIGHT, level);
-                    //     frame.setSize(WIDTH, HEIGHT);
-                    //     frame.setResizable(false);
-                    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    //     frame.setVisible(true);
+                //     // } else if (menu.getPreviousLevelPressed()) {
+                //     //     level = new LevelBoard("src/Levels/LevelTest.txt", 50,50); // Replace with previous level .txt
+                //     //     frame = new GameBoard(WIDTH, HEIGHT, level);
+                //     //     frame.setSize(WIDTH, HEIGHT);
+                //     //     frame.setResizable(false);
+                //     //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //     //     frame.setVisible(true);
 
-                    // } else if (menu.getInstructionsPressed()) {
-                    //     Instructions instructionScreen = new Instructions(WIDTH, HEIGHT);
-                    // }
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                //     // } else if (menu.getInstructionsPressed()) {
+                //     //     Instructions instructionScreen = new Instructions(WIDTH, HEIGHT);
+                //     // }
+                // } catch (IOException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // }
 
 
 
@@ -57,20 +56,22 @@ public class Main {
 
                 // // Now the menu controls the screens when buttons are pressed (calls ScreenState.java)
                 // Try reading in a Test Case
+                
+                LevelBoard level = new LevelBoard("src/Levels/Level1.txt", 100, 50);
             
-                System.out.println(level.getLevelInfo());
+                // System.out.println(level.getLevelInfo());
 
-                int indexYellowBlock1 = level.BlockIndexAt(750, 750);
+                // int indexYellowBlock1 = level.BlockIndexAt(750, 750);
 
-                level.push(indexYellowBlock1, Direction.RIGHT);
+                // level.push(indexYellowBlock1, Direction.RIGHT);
                 // int indexRedBlock1 = level.BlockIndexAt(650, 150);
                 // level.push(indexRedBlock1, Direction.LEFT);
 
                 for(int i = 0; i < 6; i++){
                     level.update();
                     System.out.println(level.getBoardGrid());
-                    System.out.println(level.getBlocks().get(indexYellowBlock1).getBlockInfo());
-                    System.out.println(level.isComplete());
+                    // System.out.println(level.getBlocks().get(indexYellowBlock1).getBlockInfo());
+                    // System.out.println(level.isComplete());
                 }
             }
         });
